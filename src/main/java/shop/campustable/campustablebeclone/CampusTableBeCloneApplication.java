d36cecfc9f60ddd0f6a65838dceba8674ab5a002
Chuseok22 <bjh59629@naver.com>
2026-01-05 13:24:42 +0900

    project init

---
+++ src/main/java/shop/campustable/campustablebeclone/CampusTableBeCloneApplication.java
@@ -0,0 +1,13 @@
+package shop.campustable.campustablebeclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusTableBeCloneApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CampusTableBeCloneApplication.class, args);
+  }
+
+}
---
+++ src/test/java/shop/campustable/campustablebeclone/CampusTableBeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.campustable.campustablebeclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusTableBeCloneApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
