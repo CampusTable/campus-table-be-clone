253fd695d6f0572f1b479c9899769add19bd2026
Chuseok22 <bjh59629@naver.com>
2026-01-05 13:30:13 +0900

    Update SpringDocProperties.java

--- src/main/java/shop/campustable/campustablebeclone/global/properties/SpringDocProperties.java
+++ src/main/java/shop/campustable/campustablebeclone/global/properties/SpringDocProperties.java
@@ -1,17 +1,21 @@
 package shop.campustable.campustablebeclone.global.properties;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
+@AllArgsConstructor
 @ConfigurationProperties(prefix = "springdoc")
 public class SpringDocProperties {
 
   List<Server> servers;
 
   @Getter
+  @AllArgsConstructor
   public static class Server {
+
     String url;
     String description;
   }
