fad35e4bc35c28c5f2d4d3e00ff345faaa76e792
kjh0718 <ranbow3062961@gmail.com>
2026-02-17 21:55:02 +0900

    주문_내역_조회_페이지네이션 : feat : 주문내역 페이지네이션 https://github.com/CampusTable/campus-table-be-clone/issues/42

--- src/main/java/shop/campustable/campustablebeclone/domain/order/repository/OrderRepository.java
+++ src/main/java/shop/campustable/campustablebeclone/domain/order/repository/OrderRepository.java
@@ -3,6 +3,8 @@ package shop.campustable.campustablebeclone.domain.order.repository;
 import jakarta.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -16,10 +18,10 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
   @Query("select o from Order o where o.id = :id")
   Optional<Order> findByIdWithForceIncrement(@Param("id") Long id);
 
-  @Query("SELECT DISTINCT o FROM Order o " +
-         "JOIN FETCH o.cafeteria " +
-         "JOIN FETCH o.orderItems " +
-         "WHERE o.user.id = :userId")
-  List<Order> findOrdersWithItemsAndCafeteriaByUserId(@Param("userId") Long userId);
+  @Query(value = "SELECT DISTINCT o FROM Order o " +
+                 "JOIN FETCH o.cafeteria " + // 1:1 관계는 Fetch Join 해도 페이징 가능!
+                 "WHERE o.user.id = :userId",
+      countQuery = "SELECT count(o) FROM Order o WHERE o.user.id = :userId")
+  Page<Order> findOrdersWithItemsAndCafeteriaByUserId(@Param("userId") Long userId, Pageable pageable);
 
 }
